ac4351105470990a67596eed51bf846ddce20457
olha.codes <57961694+olhacodes@users.noreply.github.com>
2022-10-23 15:52:15 +0300

    added fake DB, nodedemon to autorun server, company type, company type to the root type

--- schema/schema.js
+++ schema/schema.js
@@ -1,44 +1,53 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
-const users = [
-    {
-       id: '23',
-       firstName: "Olha",
-       age: 27
-    },
-    {
-       id: '47',
-       firstName: "Samanta",
-       age: 21 
-    }
-]
+const CompanyType = new GraphQLObjectType({
+	name: "Company",
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString }
+	}
+})
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt },
+			age: { type: GraphQLInt },
+			company: {
+				type: CompanyType,
+				resolve(parentValue, args) {
+					return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+					.then(res => res.data)
+				}
+			}
     }
 });
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        user: {
-            type: UserType,
-            args: {
-                id: {
-                type: GraphQLString
-                }
-            },
-            resolve(parentValue, args) {
-                return _.find(users, {id: args.id})
-            }
+      user: {
+        type: UserType,
+        args: {id: { type: GraphQLString }
+        },
+        resolve(parentValue, args) {
+            return axios.get(`http:localhost:3000/users/${args.id}`)
+              .then(resp => resp.data)
         }
+			},
+			company: {
+				type: CompanyType,
+				args: {id: { type: GraphQLString } },
+				resolve(parentValue, args) {
+					 return axios.get(`http:localhost:3000/companies/${args.id}`)
+            .then(resp => resp.data)
+				}
+			}
     }
 });
 
